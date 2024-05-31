243ed336fedfbd52e0afdb6258b4bb2f6f8813c7
BorysPal <borys.paliwoda@wp.pl>
2024-05-31 23:38:57 +0200

    Add files via upload

---
+++ kalkulator.js
@@ -0,0 +1,11 @@
+
+function facet(a,b,c,d) {
+    let wynikf = (10*a)+(6.25*b)-(5*c)+5+(1*d);
+    let element_txtWynikf= document.getElementById("txtWynikf");
+    element_txtWynikf.value=wynikf;
+}
+function kobieta(a,b,c,d) {
+    let wynikk = (10*a)+(6.25*b)-(5*c)-161+(1*d);
+    let element_txtWynikk= document.getElementById("txtWynikk");
+    element_txtWynikk.value=wynikk;
+}
